4c091453b1f45aa45b88064eb5ad9a19c1afd1da
easyscripts9 <86202469+easyscripts9@users.noreply.github.com>
2021-06-30 15:25:00 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,52 @@
+import webbrowser
+
+print ("░██████╗░░█████╗░░█████╗░░██████╗░██╗░░░░░███████╗  ████████╗░█████╗░░█████╗░██╗░░░░░░██████╗
+"██╔════╝░██╔══██╗██╔══██╗██╔════╝░██║░░░░░██╔════╝  ╚══██╔══╝██╔══██╗██╔══██╗██║░░░░░██╔════╝
+"██║░░██╗░██║░░██║██║░░██║██║░░██╗░██║░░░░░█████╗░░  ░░░██║░░░██║░░██║██║░░██║██║░░░░░╚█████╗░
+"██║░░╚██╗██║░░██║██║░░██║██║░░╚██╗██║░░░░░██╔══╝░░  ░░░██║░░░██║░░██║██║░░██║██║░░░░░░╚═══██╗
+"╚██████╔╝╚█████╔╝╚█████╔╝╚██████╔╝███████╗███████╗  ░░░██║░░░╚█████╔╝╚█████╔╝███████╗██████╔╝
+"░╚═════╝░░╚════╝░░╚════╝░░╚═════╝░╚══════╝╚══════╝  ░░░╚═╝░░░░╚════╝░░╚════╝░╚══════╝╚═════╝░")
+
+a = "¿QUE HERRAMIENTA USARAS?"
+
+b = "  (1) WIRESHARK"
+
+
+c = "  (2) INFORMACIÓN DE UN NÚMERO"
+
+
+d = "  (3)  INFO DE UNA IP :"
+
+
+e = "  (4) DNS LOOKUP:"
+
+
+f ="   (5) DNI LOOKUP"
+
+
+g = "        #:elige una opcion:"
+
+
+kpo = input (a + b + c + d + e + f + h + g)
+
+input (kpo)
+
+if kpo == "1":
+   webbrowser.open("https://www.wireshark.org/")
+
+if kpo == "2":
+    webbrowser.open("https://numverify.com/")
+
+if kpo == "3":
+webbrowser.open ("https://ipinfo.com/")
+if kpo == "4":
+webbrowser.open ("https://dns-lookup.com/")
+if kpo == "5":
+webbrowser.open ("https://www.ip-lookup.org/dns-lookup")
+if kpo == "6":
+    webbrowser.open ("https://www.dateas.com/es?country=espana&container_id=public-search&form_id=cuit")
+
+
+
+
+
